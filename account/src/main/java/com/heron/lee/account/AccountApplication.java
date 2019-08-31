6f9d007d25dde07949254ab1410c64474b28cb14
God한빈 <leehan0617@naver.com>
2019-08-31 18:18:32 +0900

    Add package

---
+++ account/src/main/java/com/heron/lee/account/AccountApplication.java
@@ -0,0 +1,8 @@
+package com.heron.lee.account;
+
+public class AccountApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Account Appication");
+    }
+}
---
+++ common/src/test/java/com/heron/lee/common/InitalTest.java
@@ -0,0 +1,11 @@
+package com.heron.lee.common;
+
+import org.junit.Test;
+
+public class InitalTest {
+
+    @Test
+    public void initTest () {
+        System.out.println("common module test");
+    }
+}
